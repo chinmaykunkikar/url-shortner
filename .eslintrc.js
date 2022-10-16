6d9e5dafac95b08cc718d8edf7dfe0e210cc796a
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-17 02:47:31 +0530

    Configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard-with-typescript", "prettier"],
+  overrides: [],
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "warn",
+  },
+};
