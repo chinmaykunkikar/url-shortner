db99b4ec3a4b2c0ab433c1c8bdc83cab9426f051
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-17 15:12:32 +0530

    Import changes to model

--- models/shortURL.model.ts
+++ models/shortURL.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 import { customAlphabet } from "nanoid";
 const nanoid = customAlphabet("1234567890abcdefghjklmnpqrstuvwxyz", 5);
 
@@ -7,7 +7,7 @@ export interface ShortURL extends Document {
   destination: string;
 }
 
-const schema = new mongoose.Schema({
+const schema = new Schema({
   shortId: {
     type: String,
     unique: true,
@@ -20,6 +20,6 @@ const schema = new mongoose.Schema({
   },
 });
 
-const shortURL = mongoose.model<ShortURL>("shortURL", schema);
+const shortURL = model<ShortURL>("shortURL", schema);
 
 export default shortURL;
