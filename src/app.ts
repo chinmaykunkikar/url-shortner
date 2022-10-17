ca5d617340d550f81994b801250cd737491d7132
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-17 15:14:36 +0530

    Parse json data using body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import config from "config";
 import routes from "../routes";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = config.get("port");
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log("App running");
