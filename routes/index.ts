6ece0a3dd37898d24a11ce912d2e186e34c144f3
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-17 15:14:16 +0530

    Add shortURL controller and API end point

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,13 @@
 import { Express, Request, Response } from "express";
+import { createShortURL } from "../controllers/shortURL.controller";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => {
     return res.send("App is running without problems.");
   });
+
+  app.post("/api/shortenurl", createShortURL);
 }
 
 export default routes;
