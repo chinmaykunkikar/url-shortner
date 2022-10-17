f6a0047f6adc69c9c53643fcc2f288012554efad
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-17 19:11:09 +0530

    MOAR eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
   plugins: ["prettier"],
   rules: {
     "prettier/prettier": "warn",
+    "no-unused-vars": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
