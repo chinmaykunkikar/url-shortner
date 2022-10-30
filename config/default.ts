997aa57fcf0c1695b099cf42eaa25ab1fb7d0256
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-30 15:11:26 +0530

    server: add cors config

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
-  port: 9090,
+  port: 8000,
   dbUri: "mongodb://localhost:27017/urlshortner",
+  corsOrigin: "http://localhost:5173",
 };
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import config from "config";
+import cors from "cors";
 import routes from "../routes";
 import bodyParser from "body-parser";
 import database from "./database";
 
 const app = express();
 const port = config.get("port");
+
+app.use(
+  cors({
+    origin: config.get("corsOrigin"),
+  }),
+);
 
 app.use(bodyParser.json());
 
